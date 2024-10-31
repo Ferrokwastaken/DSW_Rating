f44481f30d4b97a85b7027a32d788384c919464b
Ferrokwastaken <israelduquesilva@hotmail.com>
2024-10-31 16:24:18 +0000

    now with mysql

--- public/statistic.php
+++ public/statistic.php
@@ -20,28 +20,38 @@
     <tbody>
   
   <?php
-    $path = '../data/';
-    $dir = opendir($path);
-    while ($fileName = readdir($dir)) {
-    if (is_file($path . $fileName)) {
-      $fileNameWithoutExtension = explode('.', $fileName)[0];
-      list($year, $month, $day, $hour, $min) = explode('_', $fileNameWithoutExtension);
-      $date = $year . "/" . $month . "/" . $day;
-      $time = $hour . ":" . $min;
-      $content = file_get_contents($path . $fileName);
-      $rates = explode(',', $content);
-      array_pop($rates);
-      $count = count($rates);
-      $total = 0;
-      foreach ($rates as $rate) {
-        $total += $rate;
-      }
-      $avg = $total / $count;
+    require_once 'connection.php';
+
+    $results = $link->query('SELECT DATE_FORMAT(date, "%Y-%c-%d %H:%i") AS dateformat, COUNT(rate) AS count, avg(rate) AS average FROM rates GROUP BY dateformat');
+
+    while ($rate = $results->fetch(PDO::FETCH_OBJ)) {
+      list($date, $time) = explode(' ', $rate->dateformat);
       printf("<tr> <td>%s</td> <td>%s</td> <td>%d</td> <td>%.2f</td> </tr>",
-    $date, $time, $count, $avg);
+    $date, $time, $rate->count, $rate->average);
     }
-  }
-    closedir($dir);
+
+    $link = null;
+
+  //   while ($fileName = readdir($dir)) {
+  //   if (is_file($path . $fileName)) {
+  //     $fileNameWithoutExtension = explode('.', $fileName)[0];
+  //     list($year, $month, $day, $hour, $min) = explode('_', $fileNameWithoutExtension);
+  //     $date = $year . "/" . $month . "/" . $day;
+  //     $time = $hour . ":" . $min;
+  //     $content = file_get_contents($path . $fileName);
+  //     $rates = explode(',', $content);
+  //     array_pop($rates);
+  //     $count = count($rates);
+  //     $total = 0;
+  //     foreach ($rates as $rate) {
+  //       $total += $rate;
+  //     }
+  //     $avg = $total / $count;
+  //     printf("<tr> <td>%s</td> <td>%s</td> <td>%d</td> <td>%.2f</td> </tr>",
+  //   $date, $time, $count, $avg);
+  //   }
+  // }
+  //   closedir($dir);
   ?>
 
     </tbody>
