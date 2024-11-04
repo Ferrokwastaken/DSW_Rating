994458c3054c6c67125893bb21563c787215aeac
Ferrokwastaken <israelduquesilva@hotmail.com>
2024-11-04 17:16:02 +0000

    save file method

--- src/StoreFile.php
+++ src/StoreFile.php
@@ -1,10 +1,15 @@
 <?php
   namespace Dsw\Rating;
 
+use DateTime;
+
   class StoreFile implements StoreInterface {
     public function addRate(int $rate)
     {
-      
+      $date = new DateTime();
+      $path = '../data/';
+      $filename = $path . $date->format('Y_m_d_H_i') . '.csv';
+      file_put_contents($filename, $rate . ',', FILE_APPEND);
     }
 
     public function showStats(): array
